e1c3615669f82bf6256ca38bb5dee720804ab16b
dan0505 <qiaoer0505@gmail.com>
2019-12-03 12:54:54 +1100

    fiexed friendly_id

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -45,6 +45,6 @@ class BlogsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def blog_params
-    params.require(:blog).permit(:title, :body)
+    parm_tmp = params.require(:blog).permit(:title, :body)
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,4 @@
 class Blog < ApplicationRecord
   extend FriendlyId
-  friendly_id :name, use: :slugged
+  friendly_id :title, use: :slugged
 end
